66eee7bc9d331edab0b9f34a30707c7a468f52d3
Faisal-Sey <phaisalsey6@gmail.com>
2024-09-05 20:54:49 +0000

    basic functionality complete

--- products/urls/configurations/menu_urls.py
+++ products/urls/configurations/menu_urls.py
@@ -1,7 +1,21 @@
 from django.urls import path
-from products.views.configurations.menu_views import MenuListCreateAPIView, MenuRetrieveUpdateDestroyAPIView
+
+from products.views.configurations.menu_views import (
+    MenuByTitleSingleAPIView,
+    MenuGetUpdateDestroyAPIView,
+    MenuListCreateAPIView,
+)
 
 menu_routes = (
-    path('menus/',  MenuListCreateAPIView.as_view(), name='menu-list-create'),
-    path('menus/<int:pk>/', MenuRetrieveUpdateDestroyAPIView.as_view(), name='menu-detail'),
+    path("menus/", MenuListCreateAPIView.as_view(), name="menu-list-create"),
+    path(
+        "menus/title/<str:menu_title>/",
+        MenuByTitleSingleAPIView.as_view(),
+        name="menu-title-detail",
+    ),
+    path(
+        "menus/<int:pk>/",
+        MenuGetUpdateDestroyAPIView.as_view(),
+        name="menu-detail",
+    ),
 )
--- products/views/configurations/menu_views.py
+++ products/views/configurations/menu_views.py
@@ -1,12 +1,14 @@
 from typing import Any
-from rest_framework import generics
 
-from utils.logger.logger_handler import logger
+from django.http import Http404
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+
 from products.models.configurations.menus_model import Menu
 from products.serializers.configurations.menus_serializers import MenuSerializer
+from utils.logger.logger_handler import logger
 
 
-class MenuListCreateAPIView(generics.ListCreateAPIView):
+class MenuListCreateAPIView(ListCreateAPIView):  # type: ignore[misc]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
@@ -17,13 +19,31 @@ class MenuListCreateAPIView(generics.ListCreateAPIView):
         return queryset
 
 
-class MenuRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+class MenuGetUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):  # type: ignore[misc]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
     def get_object(self) -> Menu:
-        menu_id = self.kwargs.get('pk')
+        menu_id = self.kwargs.get("pk")
         logger.debug("Fetching menu with ID %s...", menu_id)
-        menu = super().get_object()
+        menu: Menu = super().get_object()
         logger.debug("Fetched menu: %s", menu)
         return menu
+
+
+class MenuByTitleSingleAPIView(RetrieveUpdateDestroyAPIView):  # type: ignore[misc]
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+    def get_object(self) -> Menu:
+        menu_title = self.kwargs.get("menu_title")
+        logger.debug("Fetching menu with title %s...", menu_title)
+
+        # Filter the queryset by name
+        try:
+            menu = self.queryset.get(title__iexact=menu_title)
+            logger.debug("Fetched menu: %s", menu)
+            return menu
+        except Menu.DoesNotExist:
+            logger.error("Menu with name %s not found", menu_title)
+            raise Http404("Menu not found")
